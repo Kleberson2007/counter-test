d82173b296e2d3328db70ab9e2f923c1e9c5261b
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2025-05-19 16:02:41 -0300

    ç

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
     <div id="counter">
       <h1>counter</h1>
       <p>{number}</p>
-        <div id="buttons">
-          <button onClick={add1}>+1</button>
-          <button onClick={sub1}>-1</button>
+        <div id="buttons-area">
+          <button className="buttons" id="plus" onClick={add1}>+1</button>
+          <button className="buttons" id="minus" onClick={sub1}>-1</button>
         </div>
     </div>
   );
